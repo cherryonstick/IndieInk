ac8df059c127f2f2240a8af1955c8c64964b1a17
cherryonstick <cherrytoeeater@gmail.com>
2026-01-20 00:43:50 -0500

    Update universal.lua

---
+++ universal.lua
@@ -0,0 +1,11 @@
+-- IndieInk Universal Hacks
+
+local UniversalTab = Window:CreateTab("Universal", 4483362458)
+
+-- Movement Section (your speed, fly, noclip code here)
+local MovementSection = UniversalTab:CreateSection("Movement")
+-- ... paste your slider/toggle/Heartbeat connections for speed/fly/noclip ...
+
+-- Player Section
+local PlayerSection = UniversalTab:CreateSection("Player")
+-- ... god mode toggle + Heartbeat ...
